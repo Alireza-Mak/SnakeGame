e6c07e4989f76a4051435b035255c691fbaaea44
Alireza Mak <info@alirezamak.com>
2024-07-29 17:31:02 -0700

    Updated SnakeModel and SnakeController: Added game loop handling and movement logic

--- src/SnakeController.java
+++ src/SnakeController.java
@@ -1,3 +1,5 @@
+import java.awt.event.*;
+
 public class SnakeController {
     private final SnakeModel snakeModel;
     private final SnakeView snakeView;
@@ -7,10 +9,19 @@ public class SnakeController {
         this.snakeModel = snakeModel;
 
         this.snakeModel.setScreenDetails(this.snakeView.getScreenProperties());
-        this.snakeModel.startGame();
+        this.snakeModel.startGame(new SnakeActionListener());
         this.snakeView.setAppleX(this.snakeModel.getAppleX());
         this.snakeView.setAppleY(this.snakeModel.getAppleY());
         this.snakeView.setSnakeX(this.snakeModel.getSnakeX());
         this.snakeView.setSnakeY(this.snakeModel.getSnakeY());
+    }
+
+
+    private class SnakeActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            snakeModel.moveSnake(snakeView.getScreenProperties().get("UNIT_SIZE"));
+            snakeView.repaint();
+        }
     }
 }
--- src/SnakeModel.java
+++ src/SnakeModel.java
@@ -1,5 +1,7 @@
+import java.awt.event.*;
 import java.util.Map;
 import java.util.Random;
+import javax.swing.*;
 
 public class SnakeModel {
     private final Random rand;
@@ -9,12 +11,23 @@ public class SnakeModel {
     private int snakeLength;
     private int[] snakeX;
     private int[] snakeY;
+    private char direction = 'R';
+    private Timer timer;
+    private int delay = 100;
 
     public SnakeModel() {
         rand = new Random();
         this.snakeLength = 4;
         this.snakeX = new int[snakeLength];
         this.snakeY = new int[snakeLength];
+    }
+
+    public void setScreenDetails(Map<String, Integer> screenProperties) {
+        this.screenProperties = screenProperties;
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
     }
 
     public int[] getSnakeX() {
@@ -33,11 +46,10 @@ public class SnakeModel {
         return appleX;
     }
 
-    public void setScreenDetails(Map<String, Integer> screenProperties) {
-        this.screenProperties = screenProperties;
-    }
 
-    public void startGame() {
+    public void startGame(ActionListener actionListener) {
+        timer = new Timer(delay, actionListener);
+        timer.start();
         createApple();
     }
 
@@ -46,5 +58,25 @@ public class SnakeModel {
         appleY = rand.nextInt(screenProperties.get("SCREEN_HEIGHT") / screenProperties.get("UNIT_SIZE")) * screenProperties.get("UNIT_SIZE");
     }
 
+    public void moveSnake(int unitSize) {
+        for (int i = snakeLength - 1; i > 0; i--) {
+            snakeX[i] = snakeX[i - 1];
+            snakeY[i] = snakeY[i - 1];
+        }
+        switch (direction) {
+            case 'R':
+                snakeX[0] += unitSize;
+                break;
+            case 'L':
+                snakeX[0] -= unitSize;
+                break;
+            case 'U':
+                snakeY[0] -= unitSize;
+                break;
 
+            case 'D':
+                snakeY[0] += unitSize;
+                break;
+        }
+    }
 }
