258bf770524493b063e295fd0ae972c7fd4946b9
Alireza Mak <info@alirezamak.com>
2024-07-29 17:08:03 -0700

    Added SnakeView class for setting up the game window and rendering properties

--- src/SnakeView.java
+++ src/SnakeView.java
@@ -1,2 +1,37 @@
-public class SnakeView {
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SnakeView extends JPanel {
+    Map<String, Integer> screenProperties = new HashMap<>() {
+        {
+            put("SCREEN_WIDTH", 800);
+            put("SCREEN_HEIGHT", 600);
+            put("UNIT_SIZE", 25);
+        }
+    };
+
+    private JFrame frame;
+
+    public SnakeView() {
+        this.setBackground(Color.WHITE);
+        this.setPreferredSize(new Dimension(screenProperties.get("SCREEN_WIDTH"), screenProperties.get("SCREEN_HEIGHT")));
+        this.setFocusable(true);
+        initialMainFrame();
+    }
+
+
+    public void initialMainFrame() {
+        frame = new JFrame("Snake Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.add(this);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+
 }
